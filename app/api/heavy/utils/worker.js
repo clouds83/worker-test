2575b1630b8e8e0627f2c6e7301b8cf4960cf6b6
Claudio Godoy <clouds83@gmail.com>
2024-09-16 10:00:42 -0400

    5

--- app/api/heavy/utils/worker.js
+++ app/api/heavy/utils/worker.js
@@ -3,7 +3,7 @@ const { parentPort } = require('worker_threads')
 // const heavyFunctionPath = path.resolve(__dirname, 'heavyFunction.js')
 // const { heavyFunction } = require(heavyFunctionPath)
 // const { heavyFunction } = require(path.resolve('./heavyFunction'))
-const { heavyFunction } = require(path.resolve('public/heavyFunction'))
+const { heavyFunction } = require(path.join(process.cwd(), 'public/heavyFunction'))
 
 parentPort.on('message', async (data) => {
   try {
