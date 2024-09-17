aa5499b449b198175a24913bbe8969aa74ecbcf2
Claudio Godoy <clouds83@gmail.com>
2024-09-17 09:02:39 -0400

    test vercel json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      'api/heavy/utils': ['./heavyFunction.js'],
-    },
-  },
+  // output: 'standalone',
+  // experimental: {
+  //   outputFileTracingIncludes: {
+  //     'api/heavy/utils': ['./heavyFunction.js'],
+  //   },
+  // },
 }
 
 module.exports = nextConfig
