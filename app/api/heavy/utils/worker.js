5a1ddeb80d51bc980c907b9e6305798d29613a88
Claudio Godoy <clouds83@gmail.com>
2024-09-17 14:19:29 -0400

    test

--- app/api/heavy/utils/worker.js
+++ app/api/heavy/utils/worker.js
@@ -19,6 +19,8 @@ const heavyFunction = require('heavyFunction.js')
 
 parentPort.on('message', async (data) => {
   try {
+    const filePath = path.resolve('.', 'public/heavyFunction.js')
+    console.log('filePath', filePath)
     // console.log('heavyFunction', path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js'))
     // const heavyFunction = await require(path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js'))
 
