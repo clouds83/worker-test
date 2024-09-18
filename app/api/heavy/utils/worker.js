ffe10e42adaaef06c9087cd0676000dec86c3352
Claudio Godoy <clouds83@gmail.com>
2024-09-18 10:12:57 -0400

    test

--- app/api/heavy/utils/worker.js
+++ app/api/heavy/utils/worker.js
@@ -1,28 +1,18 @@
 const path = require('path')
 const fs = require('fs')
 const { parentPort } = require('worker_threads')
-// const heavyFunctionPath = path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js')
-// const { heavyFunction } = require(heavyFunctionPath)
-// const { heavyFunction } = require(path.resolve('./heavyFunction'))
-// const { heavyFunction } = require(path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction'))
-// const { heavyFunction } = require(path.resolve('app/api/heavy/utils/heavyFunction'))
-// const heavyFunctionCode = fs.readFileSync(heavyFunctionPath, 'utf-8')
-// const heavyFunction = new Function('return ' + heavyFunctionCode)()
-
-// const { heavyFunction } = require('./heavyFunction.js')
-// const { heavyFunction } = require(path.resolve('./heavyFunction.js'))
-// const { heavyFunction } = require(path.join(process.cwd(), '../../app/api/heavy/utils/heavyFunction.js'))
-// const { heavyFunction } = require(path.resolve('app/api/heavy/utils/heavyFunction.js'))
-// const heavyFunction = require(path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js'))
-
-// const heavyFunction = require('heavyFunction.js')
-// const heavyFunction = require(path.join(__dirname, '/heavyFunction.js'))
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
 
 parentPort.on('message', async (data) => {
-  try {
-    // console.log('heavyFunction', path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js'))
-    // const heavyFunction = await require(path.join(process.cwd(), 'app/api/heavy/utils/heavyFunction.js'))
+  const imageUrl = `${baseUrl}/cse-logo-export-pdf.png`
 
+  const response = await fetch(imageUrl)
+  const arrayBuffer = await response.arrayBuffer() // Read image as array buffer
+  const base64Image = Buffer.from(arrayBuffer).toString('base64') // Convert to base64
+
+  console.log({ base64Image })
+
+  try {
     const result = heavyFunction(data)
     const base64 = Buffer.from(result).toString('base64')
     parentPort.postMessage({ base64 })
--- public/heavyFunction.js
+++
@@ -1,6 +0,0 @@
-function heavyFunction(multiplier) {
-  for (let i = 0; i < 1000000000 * multiplier; i++) {}
-  return 'Heavy process done!'
-}
-
-module.exports = heavyFunction
